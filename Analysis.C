c70347cafb20dc41b6ce166574a670f8a3ae2b18
Jay <jlawhorn@mit.edu>
2016-05-18 18:23:17 +0200

    updating uncertainties

--- Analysis.C
+++ Analysis.C
@@ -1,9 +1,13 @@
 #include "TROOT.h"
 #include "TStyle.h"
+#include "TAxis.h"
 #include "TFile.h"
 #include "TGraph.h"
 #include "TGraphErrors.h"
 #include "TF1.h"
+#include "TCanvas.h"
+#include "TPaveText.h"
+#include "TLegend.h"
 
 #include <iostream>
 #include <sstream>
@@ -19,6 +23,15 @@
 #include "Math/Functor.h"
 
 using namespace std;
+
+//static const double tzero[3]= {23.960177, 13.307784, 9.109694};
+//static const double slope[3] = {-3.178648,  -1.556668, -1.075824 };
+//static const double tmax[3] = {16.00, 10.00, 6.25 };
+//
+//double delay(double fC, BiasSetting bias) {
+//  double rawDelay=tzero[bias]+slope[bias]*log(fC);
+//  return (rawDelay<0)?(0):((rawDelay>tmax[bias])?(tmax[bias]):(rawDelay));   
+//}
 
 class PulseSimple 
 {
@@ -41,6 +54,8 @@ public:
       dataPulse[i]=y;
       dataErr[i]=gr.GetErrorY(i);
     }
+    double rawtslew=13.307784-1.556668*log(dataPulse[4]);
+    tslew=(rawtslew<0)?(0):((rawtslew>10)?(10):(rawtslew));
   }
 
   double operator()(const double *t) {
@@ -54,13 +69,13 @@ public:
 
   double evalPulse(double *x, double *t) {
     double ans=0;
-
+    
     double thpd=t[0];
     HcalPulseShapes thePulses_;
     
     thePulses_.computeHPDShape(ts1,ts2,ts3,thpd,tpre,wd1,wd2,wd3, thePulses_.hpdShape_);
     for (UInt_t j=0; j<25; j++) {
-      ans+=thePulses_.hpdShape_(25*floor(x[0]+0.5)+j-92.5);
+      ans+=thePulses_.hpdShape_(25*floor(x[0]+0.5)+j-92.5-tslew);
     }
     
     return ans;
@@ -68,27 +83,35 @@ public:
 
 private:
   float ts1, ts2, ts3, tpre, wd1, wd2, wd3;
+  float tslew;
   double dataPulse[10];
   double dataErr[10];
 };
 
+
 void PrintPulse(float thpd);
 
-void Analysis() {
+void Analysis(Int_t i=0) {
+
+  char gname[50];
+  char pname[50];
+
+  sprintf(gname, "graph_%i",i);
+  sprintf(pname, "bin_%i.png",i);
 
   TFile *infile = new TFile("test_hist.root");
-  TGraphErrors *g = (TGraphErrors*) infile->Get("gr");
+  TGraphErrors *g = (TGraphErrors*) infile->Get(gname);
 
   ROOT::Math::Minimizer *min = ROOT::Math::Factory::CreateMinimizer("Minuit2","");
-  min->SetMaxFunctionCalls(10000000);
-  min->SetTolerance(10.0);
+  min->SetMaxFunctionCalls(100000000);
+  min->SetTolerance(100.0);
   min->SetPrintLevel(0);
 
   PulseSimple pulse = PulseSimple();
   pulse.setDataPulse(*g);
 
-  double step[1] = {0.1};
-  double variable[1] = {4.0};
+  double step[1] = {0.01};
+  double variable[1] = {2.51};
 
   ROOT::Math::Functor f(pulse,1);
   
@@ -99,6 +122,9 @@ void Analysis() {
 
   cout << *min->X() << ", " << min->MinValue() << endl;
 
+  char fval[100];
+  sprintf(fval, "t_{hpd}=%.3f", *min->X());
+
   //cout << pulse(&t) << endl;
 
   TGraph *gfit = new TGraph(0);
@@ -108,17 +134,38 @@ void Analysis() {
     gfit->SetPoint(i,i, pulse.evalPulse(&tmpX, &tmpT));
   }
 
+  TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+  TLegend *l = new TLegend(0.75, 0.75, 0.9, 0.9);
+  l->SetShadowColor(0); l->SetFillColor(0);
+
+  TPaveText *pt = new TPaveText(0.15,0.8,0.3,0.89,"NDC");
+  pt->SetShadowColor(0); pt->SetFillColor(0); pt->SetLineColor(0);
+  pt->AddText(fval);
+
   g->SetMarkerColor(kBlack);
   g->SetMarkerStyle(20);
   //g->SetMarkerSize(20);
   g->SetLineColor(kBlack);
 
+  l->AddEntry(g, "Data", "p");
+
   gfit->SetMarkerColor(kRed);
   gfit->SetMarkerStyle(20);
   gfit->SetLineColor(kRed);
 
+  l->AddEntry(gfit, "Fit", "p");
+
+  g->GetYaxis()->SetRangeUser(0,1);
+  g->GetYaxis()->SetTitle("Normalized Pulse Shape");
+  g->GetXaxis()->SetTitle("Time Slice");
+  g->SetTitle("");
   g->Draw("ape");
   gfit->Draw("p same");
+
+  l->Draw();
+  pt->Draw();
+
+  c1->SaveAs(pname);
 
   //PrintPulse(*min->X());
 
--- draw.C
+++ draw.C
@@ -22,18 +22,58 @@ double sigma(double ifC) {
 void draw() {
 
   TChain *chain = new TChain("Events");
-  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_3.root");
-  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_30.root");
-  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_31.root");
-  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_32.root");
-  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_33.root");
-  //chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_34.root");
-  //chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_35.root");
-  //chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_36.root");
-  //chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_37.root");
-  //chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_38.root");
-  //chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_39.root");
 
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_1.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_10.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_100.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_101.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_102.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_103.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_104.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_105.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_106.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_107.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_108.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_109.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_11.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_110.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_111.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_112.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_113.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_114.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_115.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_116.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_117.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_118.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_119.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_12.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_120.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_121.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_122.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_123.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_124.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_125.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_126.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_127.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_128.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_129.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_13.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_130.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_131.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_132.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_133.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_134.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_135.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_136.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_137.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_138.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_139.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_14.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_140.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_141.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_142.root");
+  chain->AddFile("/afs/cern.ch/work/j/jlawhorn/HighPtJet80/HCALTree_143.root");
+  
   cout << chain->GetEntries() << endl;
 
   Double_t pulse[10], ped[10];
@@ -41,81 +81,88 @@ void draw() {
   chain->SetBranchAddress("pulse", &pulse);
   chain->SetBranchAddress("ped", &ped);
 
-  Double_t avgPulse[10], avgUnc[10]; 
-  for (Int_t i=0; i<10; i++) {avgPulse[i]=0; avgUnc[i]=0;}
+  vector<Double_t> ucut;
+  vector<Double_t> lcut;
 
-  Int_t nPulses=0;
+  const int nbin=11;
+
+  for (int i=0; i<nbin; i++) {
+    lcut.push_back(25+i*25);
+    ucut.push_back(50+i*25);
+  }
+  
+  Double_t avgPulse[nbin][10], avgPulse2[nbin][10], adcUnc[nbin][10], totUnc[nbin][10]; 
+  for (UInt_t i=0; i<nbin; i++) {
+    for (UInt_t j=0; j<10; j++) {avgPulse[i][j]=0; avgPulse2[i][j]=0; adcUnc[i][j]=0; totUnc[i][j]=0;}
+  }
+
+  Int_t nPulses[nbin]={0};
+
+  TFile *outf = new TFile("test_hist.root", "recreate");
+  TH1D *hQ = new TH1D("hq", "hq",50, 0, 400);
 
   for (UInt_t i=0; i<chain->GetEntries(); i++) {
+  //for (UInt_t i=0; i<1000; i++) {
     chain->GetEntry(i);
 
     Double_t sumQ=0;
-    for (UInt_t j=0; j<10; j++) { sumQ+=(pulse[j]-ped[j]); }
-    //if (sumQ<100 || sumQ>120) continue;
-    if (sumQ<60 || sumQ>80) continue;
-    //if (sumQ<200 || sumQ>250) continue;
+    for (UInt_t j=0; j<10; j++) { sumQ+=pulse[j];}
 
-    nPulses++;
+    hQ->Fill(sumQ);
+
+    if (sumQ<25 || sumQ>300) continue;
+    Int_t ibin= floor( sumQ/25-1 );
+    //cout << sumQ << ", " << ibin << endl;
+
+    nPulses[ibin]++;
 
     Double_t tmpUnc[10];
     Double_t uncNorm=0;
 
     for (UInt_t j=0; j<10; j++) { 
-      tmpUnc[j]=sqrt(sigma(pulse[j]-ped[j])*sigma(pulse[j]-ped[j])+1);
+      tmpUnc[j]=sqrt(sigma(pulse[j])*sigma(pulse[j])+1);
       uncNorm+=tmpUnc[j];
     }
 
     for (UInt_t j=0; j<10; j++) { 
       tmpUnc[j]/=uncNorm;
 
-      avgPulse[j]+=(pulse[j]-ped[j])/sumQ;
-      avgUnc[j]+=tmpUnc[j]*tmpUnc[j];
+      avgPulse[ibin][j]+=pulse[j]/sumQ;
+      avgPulse2[ibin][j]+=pulse[j]*pulse[j]/sumQ;
+      adcUnc[ibin][j]+=tmpUnc[j]*tmpUnc[j];
     }
 
   }
 
-  TFile *outf = new TFile("test_hist.root", "recreate");
+  //hQ->Draw("hist");
+
   //TH1D *h = new TH1D("h1", "h1", 10, -0.5, 9.5);
-  TGraphErrors *g = new TGraphErrors(0);
+  vector<TGraphErrors*> grs;
+  for (UInt_t i=0; i<nbin; i++) {
+    char gname[50];
+    sprintf(gname,"graph_%i",i);
+    grs.push_back(new TGraphErrors(0));
+    grs[i]->SetName(gname);
 
-  cout << nPulses << endl;
+    cout << lcut[i] << " < Q_10 < " << ucut[i] << ": " << nPulses[i] << endl;
 
-  for (UInt_t j=0; j<10; j++) {
-    avgPulse[j]/=nPulses;
-    avgUnc[j]=sqrt(avgUnc[j])/nPulses;
+    for (UInt_t j=0; j<10; j++) {
+      avgPulse[i][j]/=nPulses[i];
+      avgPulse2[i][j]/=nPulses[i];
+      adcUnc[i][j]=sqrt(adcUnc[i][j])/nPulses[i];
+      totUnc[i][j]=sqrt(adcUnc[i][j] + avgPulse2[i][j] - avgPulse[i][j]*avgPulse[i][j])/sqrt(nPulses[i]);
 
-    cout << "avgPulse[" << j << "] = " << avgPulse[j] << " +/- " << avgUnc[j] << endl;
+      //cout << "avgPulse[" << j << "] = " << avgPulse[j] << " +/- " << totUnc[j] << endl;
 
-    //h->SetBinContent(j+1, avgPulse[j]);
-    g->SetPoint(j, j, avgPulse[j]);
-    g->SetPointError(j, 0, avgUnc[j]);
+      grs[i]->SetPoint(j, j, avgPulse[i][j]);
+      grs[i]->SetPointError(j, 0, totUnc[i][j]);
+    }
+    
+    grs[i]->Write();
   }
 
-  g->SetName("gr"); g->Write();
-  //cout << h->Integral() << endl;
-  
   outf->Write();
   outf->Close();
 
-
-  //vector<TH1D*> h;
-  //vector<Double_t> ucut;
-  //vector<Double_t> lcut;
-  
-  //for (int i=0; i<10; i++) {
-  //char hname[50], cut[200];
-  
-  //sprintf(hname, "h%i", i);
-  //h.push_back(new TH1D(hname, hname, 10,-0.5,9.5));
-  //
-  //lcut.push_back(25+i*25);
-  //ucut.push_back(50+i*25);
-  
-  //sprintf(cut,"pulse*(m2Charge>%.1f && m2Charge<%.1f)",lcut[i],ucut[i]);
-  //t->Draw(TString("TS>>")+TString(hname), cut);
-  //h[i]->Scale(1.0/h[i]->Integral());
-
-  //outf->Write();
-  //outf->Close();
 
 }
